213d6e42f42238af53a0cf1d843ff5f4ccbf6492
Arafat MShuvo <arafatshuvo18@gmail.com>
2022-04-27 15:43:37 +0600

    initial project done

--- js/app.js
+++ js/app.js
@@ -12,6 +12,40 @@ document.getElementById('calculate').addEventListener('click', function () {
     const businessValue = parseFloat(business.value);
     business.value = '';
 
+    
     const fullValue = bankValue + goldValue + silverValue + businessValue;
-    console.log(fullValue)
+    const balanace = document.getElementById('zakat');
+    balanace.innerText = fullValue;
+});
+document.getElementById('save').addEventListener('click', () => {
+    const total = document.getElementById('zakat');
+    const valueTotal = parseFloat(total.innerText);
+    
+    const payable = (2.58 / 100) * valueTotal;
+    console.log(payable)
+
+    const pay = document.getElementById('pay');
+    pay.innerText = payable;
+});
+
+
+document.getElementById('goods-save').addEventListener('click', () => {
+    const goods = document.getElementById('goods');
+    const goodsValue = parseFloat(goods.value);
+    const saving = (5 / 100) * goodsValue;
+    const goodsItem = document.getElementById('goods-value');
+    goodsItem.innerText = saving;
+});
+
+document.getElementById('total-value').addEventListener('click', () => {
+    console.log(555);
+    const payZakat = document.getElementById('pay');
+    const payZakatValue = parseInt(payZakat.innerText);
+    const goodsItemValue = document.getElementById('goods-value');
+    const value = parseInt(goodsItemValue.innerText)
+    const finalZakat = value + payZakatValue;
+    const totalzakat = document.getElementById('total-zakat');
+    totalzakat.innerText = finalZakat;
+    console.log(finalZakat)
 })
+
